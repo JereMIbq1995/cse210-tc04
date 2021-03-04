248ef79bee0598028c412c403bf6ef7a4378480a
Abram Hansen <hansenabram@gmail.com>
2021-03-04 16:40:03 -0700

    Wrote the doUpdates function

--- game/director.py
+++ game/director.py
@@ -11,5 +11,12 @@ class Director:
     def getInput(self):
         pass
 
-    def doUpdates(self):
-        pass
+    def doUpdates(self, guess):
+        if self.current_card < self.next_card and guess == "h":
+            self.points += 100
+        elif self.current_card < self.next_card and guess == "l":
+            self.points -= 75
+        elif self.current_card > self.next_card and guess == "l":
+            self.points += 100
+        elif self.current_card > self.next_card and guess == "h":
+            self.points -= 75
